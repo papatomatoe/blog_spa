d59fa7d012f5833f2e0d1d0939d8effa27f43417
papatomatoe <papatomatoe.project@gmail.com>
2022-06-11 02:22:34 +0300

    Добавляет конфиги продакшн сборки

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,145 @@
 const path = require('path');
+const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const { version } = require('./package.json');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(t|j)sx?$/i,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(c|sa)ss$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                auto: true,
-                mode: 'pure',
-                localIdentName: '[name]__[local]--[hash:base64:8]',
-              },
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: ['autoprefixer'],
-              },
-            },
-          },
-          'sass-loader',
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './public/index.html',
-      inject: 'body',
-      title: 'Blog',
-    }),
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    historyApiFallback: true,
-    port: 8080,
-    hot: true,
-    open: true,
-  },
+const isProduction = process.env.NODE_ENV === 'production';
+
+const styleLoadersConfig = [
+	{
+		loader: 'css-loader',
+		options: {
+			modules: {
+				auto: /\.module.(c|sa|sc)ss$/,
+				mode: 'pure',
+				localIdentName: '[name]__[local]--[hash:base64:8]',
+			},
+		},
+	},
+	{
+		loader: 'postcss-loader',
+		options: {
+			postcssOptions: {
+				plugins: ['autoprefixer'],
+			},
+		},
+	},
+];
+
+const config = {
+	entry: './src/index.tsx',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: `v${version}_bundle_[contenthash].js`,
+		publicPath: '/',
+		clean: true,
+		crossOriginLoading: 'anonymous',
+		module: true,
+	},
+	experiments: {
+		topLevelAwait: true,
+		outputModule: true,
+	},
+	devServer: {
+		compress: true,
+		historyApiFallback: true,
+		port: '8000',
+		hot: true,
+		open: true,
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+		plugins: [new TsconfigPathsPlugin()],
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, './public/assets'),
+					to: 'assets',
+					noErrorOnMissing: true,
+				},
+			],
+		}),
+		new HTMLWebpackPlugin({
+			inject: 'body',
+			template: './public/index.html',
+			title: 'Test',
+		}),
+		new webpack.ProvidePlugin({
+			React: 'react',
+		}),
+		new MiniCssExtractPlugin({ filename: '[name]_[contenthash].css', chunkFilename: '[id].css' }),
+		new ImageminPlugin({
+			test: /\.(jpe?g|png|gif|svg)$/i,
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', ...styleLoadersConfig],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', ...styleLoadersConfig, 'sass-loader'],
+			},
+			{
+				test: /\.less$/i,
+				use: [
+					isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+					...styleLoadersConfig,
+					{
+						loader: 'less-loader',
+						options: {
+							lessOptions: {
+								math: 'always',
+								javascriptEnabled: true,
+							},
+						},
+					},
+				],
+			},
+			{
+				test: /\.[jt]sx?$/i,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: 'asset',
+			},
+		],
+	},
+};
+
+module.exports = () => {
+	if (isProduction) {
+		config.mode = 'production';
+		config.devtool = false;
+		config.optimization = {
+			minimize: true,
+			minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+		};
+		config.entry = {
+			index: {
+				import: `./src/index.tsx`,
+				dependOn: ['react'],
+			},
+			react: ['react', 'react-dom'],
+		};
+	} else {
+		config.mode = 'development';
+		config.devtool = 'inline-source-map';
+		config.plugins.push(new webpack.HotModuleReplacementPlugin());
+	}
+
+	return config;
 };
